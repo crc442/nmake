6fe604296867a7112eac32fce00c41c6f0b97bf4
Chirag Chauhan <crc442@nyu.edu>
2024-04-09 21:23:28 -0400

    some formatting

--- help/index.js
+++ help/index.js
@@ -1,29 +1,25 @@
 exports.outputAllShort = function (nodes) {
-	const comments = filterComments(nodes)
-	if (!comments) {
-		return
-	}
-	process.stdout.write("\n");
-	comments.forEach(c => {
-		let shortComment = firstLine(c.comment);
-		process.stdout.write(`${c.target}		`);
-		process.stdout.write(`${shortComment} \n`);
-	})
-}
+  const comments = filterComments(nodes);
+  if (!comments) {
+    return;
+  }
+  process.stdout.write("\n");
+  comments.forEach((c) => {
+    let shortComment = firstLine(c.comment);
+    process.stdout.write(`${c.target}		`);
+    process.stdout.write(`${shortComment} \n`);
+  });
+};
 
 exports.outputTargetLong = function (nodes, target) {
-	const targetNode = nodes.filter(node => node.target === target)
-	if (!targetNode.length) {
-		return
-	}
-	process.stdout.write("\n")
-	process.stdout.write(`${targetNode[0].comment} \n`);
-}
+  const targetNode = nodes.filter((node) => node.target === target);
+  if (!targetNode.length) {
+    return;
+  }
+  process.stdout.write("\n");
+  process.stdout.write(`${targetNode[0].comment} \n`);
+};
 
-const filterComments = (nodes) => {
-	return nodes.filter(node => node.target !== null)
-}
+const filterComments = (nodes) => nodes.filter((node) => node.target !== null);
 
-const firstLine = (line) => { 
-	return line.split('\n')[0] 
-}
+const firstLine = (line) => line.split("\n")[0];
